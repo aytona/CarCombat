42225bce66c182125c4ff82f5570206c7a0f9028
Christopher Aytona <christopher.aytona@hotmail.com>
2015-09-24 13:50:54 -0400

    Added a script that will generate objects between 2 transforms

---
+++ Assets/Scripts/GenerateObjectsInArea.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateObjectsInArea : MonoBehaviour {
+
+	public Transform startPosition;
+	public Transform endPosition;
+	public float length;
+	public GameObject[] prefabArray;
+
+	private List<Vector3> pointsArray;
+	private Vector3 point;
+	private float distanceBetween;
+	private float pointCounter;
+
+	void Start()
+	{
+		pointsArray = new List<Vector3>();
+		distanceBetween += Vector3.Distance(startPosition.transform.position, endPosition.transform.position);
+
+		while (pointCounter < distanceBetween)
+		{
+			point += new Vector3(0, 0, length);
+			pointsArray.Add(point);
+			pointCounter += length;
+		}
+
+		for (int i = 0; i < pointsArray.Count; i++)
+		{
+			Instantiate(prefabArray[Random.Range(0, prefabArray.Length)], (Vector3)pointsArray[i] + startPosition.position, startPosition.transform.rotation);
+		}
+	}
+}
