bc9370b41f36a7182286a94d2d1a5a0fd56d1922
aytona <christopher.aytona@gmail.com>
2015-10-12 19:15:29 -0400

    UI script added

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+    public Text Timer;
+    public Text TurretCounter;
+    public Text PlayerHealth;
+
+    void Update()
+    {
+        Timer.text = FormatTime(GameManager.Instance.TimeRemaining);
+        TurretCounter.text = GameManager.Instance.TurretsRemaining.ToString();
+        PlayerHealth.text = GameManager.Instance.PlayerHealth.ToString();
+    }
+
+    private string FormatTime(float timeinSeconds)
+    {
+        return string.Format("{0}:{1:00}", Mathf.FloorToInt(timeinSeconds / 60), Mathf.FloorToInt(timeinSeconds % 60));
+    }
+}
