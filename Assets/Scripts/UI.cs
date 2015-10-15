6c0ea472cb475414f4dd2916d97f02f10fb73d07
Christopher Aytona <christopher.aytona@hotmail.com>
2015-10-15 10:08:53 -0400

    PauseMenu implemented in game scene
    - TODO: save player position

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -7,11 +7,18 @@ public class UI : Singleton<UI> {
     public Text Timer;
     public Text TurretCounter;
     public Text PlayerHealth;
+    public Text Mute;
+
+    private GameObject PauseMenu;
+    private bool paused;
+    private bool muted;
 
     void Start()
     {
         GameObject[] turrets = GameObject.FindGameObjectsWithTag("Turret");
         GameManager.Instance.TurretsRemaining = turrets.Length;
+        paused = false;
+        PauseMenu = GameObject.Find("PauseMenu");
     }
 
     void Update()
@@ -19,6 +26,63 @@ public class UI : Singleton<UI> {
         Timer.text = FormatTime(GameManager.Instance.TimeRemaining);
         TurretCounter.text = "Turrets: " + GameManager.Instance.TurretsRemaining.ToString();
         PlayerHealth.text = "Health: " + GameManager.Instance.PlayerHealth.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            paused = !paused;
+        
+        if (muted)
+        {
+            AudioListener.volume = 0;
+            Mute.text = "Unmute";
+        }
+        else if (!muted)
+        {
+            AudioListener.volume = 1;
+            Mute.text = "Mute";
+        }
+
+        if (paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if(!paused)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void Muted()
+    {
+        muted = !muted;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Save Game Test
+    public void Save()
+    {
+        PlayerPrefs.SetInt("CurrentSceneSave", Application.loadedLevel);
+
+    }
+
+    public void Load()
+    {
+        Application.LoadLevel(PlayerPrefs.GetInt("CurrentSceneSave"));
     }
 
     private string FormatTime(float timeinSeconds)
