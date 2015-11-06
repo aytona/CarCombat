5d69c317afa337abb5942a771ea2bb1bc6bf0103
aytona <christopher.aytona@gmail.com>
2015-11-05 22:56:25 -0500

    Unflip script added

---
+++ Assets/Scripts/Unflip.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unflip : MonoBehaviour {
+
+    private Quaternion initialRotation;
+    private bool onGround;
+    void Start()
+    {
+        initialRotation = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.rotation.z >= 180 && transform.rotation.z < 360 && onGround)
+            transform.rotation = initialRotation;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ground")
+            onGround = true;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            onGround = false;
+    }
+}
