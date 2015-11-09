77a8fd0fb03805d86c4709f087ad42a4d18a3d41
Christopher Aytona <christopher.aytona@hotmail.com>
2015-11-09 10:17:44 -0500

    AutoCam test

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CameraRig : TargetFollower {
+
+    protected Transform _Cam;
+    protected Transform _Pivot;
+    protected Vector3 _LastTargetPosition;
+
+    protected virtual void Awake()
+    {
+        _Cam = GetComponent<Camera>().transform;
+        _Pivot = _Cam.parent;
+    }
+}
