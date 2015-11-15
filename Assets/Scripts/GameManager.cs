76f9bc8c8971ae47418fc28f7c173dc787cddf5f
aytona <christopher.aytona@gmail.com>
2015-11-15 01:09:14 -0500

    Revert "Imported explosion fx"
    This reverts commit c95bc41c341105361e8d79adc25ce19886bdc933.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager> {
+
+	private float InvulnDelay = 3f;
+	private float timeRemaining;
+    private float maxTime = 5 * 60;
+	private int playerHealth;
+	private int turretsRemaining;
+	private int maxHealth = 10;
+	private bool isInvulnerable = false;
+    private bool hasSave = false;
+
+	public float TimeRemaining
+	{
+		get
+		{
+			return timeRemaining;
+		}
+		set
+		{
+			timeRemaining = value;
+		}
+	}
+
+	public int TurretsRemaining
+	{
+		get
+		{
+			return turretsRemaining;
+		}
+		set
+		{
+			turretsRemaining = value;
+		}
+	}
+
+	public int PlayerHealth
+	{
+		get
+		{
+			return playerHealth;
+		}
+		set
+		{
+			playerHealth = value;
+		}
+	}
+
+    public bool HasSave
+    {
+        get
+        {
+            return hasSave;
+        }
+        set
+        {
+            hasSave = value;
+        }
+    }
+
+	private void DecrementPlayerHealth(GameObject player)
+	{
+		if (isInvulnerable)
+			return;
+		StartCoroutine(InvulnerableDelay(InvulnDelay));
+		PlayerHealth--;
+		// TODO: Destroy player when health runs out
+		if (PlayerHealth <= 0)
+			Restart();
+	}
+
+	private void Restart()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+		TimeRemaining = maxTime;
+		PlayerHealth = maxHealth;
+	}
+
+    void Start()
+    {
+        TimeRemaining = maxTime;
+        PlayerHealth = maxHealth;
+    }
+
+	void Update()
+	{
+		TimeRemaining -= Time.deltaTime;
+		// TODO: Destroy player when time runs out
+		if (TimeRemaining <= 0)
+			Restart();
+	}
+	
+	private IEnumerator InvulnerableDelay(float InvulnDelay)
+	{
+		isInvulnerable = true;
+		yield return new WaitForSeconds(InvulnDelay);
+		isInvulnerable = false;
+	}
+}
---
+++ Assets/Scripts/RandomPrefabGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPrefabGenerator : MonoBehaviour {
+
+	public GameObject[] prefabArray;
+
+	private Vector3 currentPosition;
+	private Quaternion currentRotation;
+
+	void Start()
+	{
+		GetCurrentTransform();
+		GenerateNewObject();
+	}
+
+	void GetCurrentTransform()
+	{
+		currentPosition = this.transform.position;
+		currentRotation = this.transform.rotation;
+	}
+
+	void GenerateNewObject()
+	{
+		GameObject newPrefab = Instantiate(prefabArray[Random.Range(0, prefabArray.Length)]) as GameObject;
+		newPrefab.transform.position = currentPosition;
+		newPrefab.transform.rotation = currentRotation;
+	}
+}
