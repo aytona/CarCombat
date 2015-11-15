b4ca4101c80cd2ee68157ffcfed86e5b7351d72f
aytona <christopher.aytona@gmail.com>
2015-11-15 15:27:24 -0500

    Boosters available - needs to be smoothen out

---
+++ Assets/Scripts/Misc/Boost.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boost : MonoBehaviour {
+
+    [SerializeField]
+    private float boostForce;
+
+	void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+            other.GetComponent<Rigidbody>().AddForce(-transform.right * boostForce);
+    }
+}
