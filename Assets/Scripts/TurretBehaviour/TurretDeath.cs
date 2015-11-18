baa6b0e5f49faad879f2e3967e2d000cd4af078e
Christopher Aytona <christopher.aytona@hotmail.com>
2015-11-18 15:31:24 -0500

    sample turretdeath
    - first toggle if the turret is a support turret or not
    - if it is a support, it needs to toggle the attached game object
    - instantiate explosion prefab
    - at the end of explosion prefab instantiate a random pickup object

--- Assets/Scripts/TurretBehaviour/TurretDeath.cs
+++ Assets/Scripts/TurretBehaviour/TurretDeath.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class TurretDeath : MonoBehaviour {
 
+    public bool isSupport;
+
     [SerializeField]
     private GameObject explosion;
+    [SerializeField]
+    private GameObject objectAfterDeath;
 
     private float length;
+    private TurretSupportTrigger support;
 
 	void Start()
     {
+        support = GetComponent<TurretSupportTrigger>();
+        StartCoroutine(DeathAnim(length));
+    }
 
+    private IEnumerator DeathAnim(float delay)
+    {
+        Instantiate(explosion, Vector3.zero, Quaternion.identity);
+        yield return new WaitForSeconds(delay);
+        if (isSupport)
+            support.enabled = true;
     }
 }
---
+++ Assets/Scripts/TurretBehaviour/TurretSupportTrigger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretSupportTrigger : MonoBehaviour {
+
+    public GameObject objectToTrigger;
+
+
+}
