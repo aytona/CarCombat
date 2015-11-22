7febbd688b03f1f006a572688bb9da436083f197
aytona <christopher.aytona@gmail.com>
2015-11-21 23:12:17 -0500

    Updated bullet script

--- Assets/Scripts/Misc/Bullet.cs
+++ Assets/Scripts/Misc/Bullet.cs
@@ -3,8 +3,24 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
+    public GameObject explosion;
+
+    private float speed = 20f;
+    private float lifeSpan = 3f;
+    
+    void Start()
+    {
+        Destroy(gameObject, lifeSpan);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
