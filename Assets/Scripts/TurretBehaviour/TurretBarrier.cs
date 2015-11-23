f3652d08131f7b1df5ecd4becd1e6e47f10e0a94
aytona <christopher.aytona@gmail.com>
2015-11-22 23:33:11 -0500

    turret barrier object attached to boss

---
+++ Assets/Scripts/TurretBehaviour/TurretBarrier.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretBarrier : MonoBehaviour {
+
+    public GameObject[] barriers;
+
+    [SerializeField]
+    private float speed = 0.1f;
+    [SerializeField]
+    private float barrierCooldDown = 3f;
+
+    private bool toggle = true;
+
+    void Start()
+    {
+        StartCoroutine(ToggleBarrier(barrierCooldDown));
+    }
+
+    void Update()
+    {
+        transform.Rotate(transform.up, speed * 360 * Time.deltaTime);
+    }
+
+    private IEnumerator ToggleBarrier(float delay)
+    {
+        while (true)
+        {
+            foreach (GameObject barrier in barriers)
+            {
+                barrier.SetActive(toggle);
+            }
+            yield return new WaitForSeconds(delay);
+            toggle = !toggle;
+        }
+    }
+}
